0a72fe7f73f29853d845a365162c33d5643f55dd
Dominik Horn <dominik.horn@tum.de>
2021-11-13 17:52:03 +0100

    remove docs/ as they are useless (for now)

--- doc.sh
+++ doc.sh
@@ -17,15 +17,11 @@ mkdir -p ${SYMBOL_GRAPHS_DIR} && \
 ## 1.2 rm files that don't belong to this project
 find .build/symbol-graphs/* | grep -v ASN1Parser | xargs rm
 
-# 2. Export as HTML
+# 2. Preview as HTML
 DOCC=$(xcrun --find docc)
 DOCC_HTML_DIR="$(dirname "${DOCC}")/../share/docc/render"
-$DOCC convert Sources/ASN1Parser/ASN1Parser.docc \
+$DOCC preview Sources/ASN1Parser/ASN1Parser.docc \
   --fallback-display-name ASN1Parser \
   --fallback-bundle-identifier de.dhorn.ASN1Parser \
   --fallback-bundle-version 0.0.1 \
   --additional-symbol-graph-dir ${SYMBOL_GRAPHS_DIR}
-
-# 3. Move html to gh pages location
-rm -rf docs
-cp -r Sources/ASN1Parser/ASN1Parser.docc/.docc-build docs
