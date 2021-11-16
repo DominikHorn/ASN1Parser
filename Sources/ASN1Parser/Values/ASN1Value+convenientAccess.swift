f23833e3febf9659ad3c1ffc70a2c655672acfa9
Dominik Horn <dominik.horn@tum.de>
2021-11-16 21:58:53 +0100

    reference concrete ASN1Value in convenience docstring

--- Sources/ASN1Parser/Values/ASN1Value+convenientAccess.swift
+++ Sources/ASN1Parser/Values/ASN1Value+convenientAccess.swift
@@ -15,42 +15,42 @@ public extension ASN1Value {
     return casted
   }
   
-  /// Try casting to ASN1Null. Throws if cast fails
+  /// Try casting to ``ASN1Null`` Throws if cast fails
   var asNull: ASN1Null {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1Bool. Throws if cast fails
+  /// Try casting to ``ASN1Bool`` Throws if cast fails
   var asBool: ASN1Boolean {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1Integer. Throws if cast fails
+  /// Try casting to ``ASN1Integer`` Throws if cast fails
   var asInt: ASN1Integer {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1ObjectIdentifier. Throws if cast fails
+  /// Try casting to ``ASN1ObjectIdentifier`` Throws if cast fails
   var asObjectIdentifier: ASN1ObjectIdentifier {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1BitString. Throws if cast fails
+  /// Try casting to ``ASN1BitString`` Throws if cast fails
   var asBitString: ASN1BitString {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1OctetString. Throws if cast fails
+  /// Try casting to ``ASN1OctetString`` Throws if cast fails
   var asOctetString: ASN1OctetString {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1UTF8String. Throws if cast fails
+  /// Try casting to ``ASN1UTF8String`` Throws if cast fails
   var asUTF8String: ASN1UTF8String {
     get throws { try cast(self) }
   }
   
-  /// Try casting to ASN1Sequence. Throws if cast fails
+  /// Try casting to ``ASN1Sequence`` Throws if cast fails
   var asSequence: ASN1Sequence {
     get throws { try cast(self) }
   }
